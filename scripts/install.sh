a7adb4f5256bc2ff7f11433e26d32b29b76a8edb
jasonzeng <zenghongtu@gmail.com>
2023-09-04 08:31:07 +0800

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -32,8 +32,8 @@ echo "Cloning ControlNet extension repo"
 git clone https://github.com/Mikubill/sd-webui-controlnet.git extensions/sd-webui-controlnet
 
 echo "Installing dependencies for ControlNet"
-cd extensions/sd-webui-controlnet
-pip install -r requirements.txt
+# cd extensions/sd-webui-controlnet
+# pip install -r requirements.txt
 
 echo "Installing RunPod Serverless dependencies"
 cd /workspace/stable-diffusion-webui
@@ -41,23 +41,23 @@ pip3 install huggingface_hub runpod
 
 echo "Downloading Deliberate v2 model"
 cd /workspace/stable-diffusion-webui/models/Stable-diffusion
-wget -O deliberate_v2.safetensors https://civitai.com/api/download/models/15236
+wget -O awpainting_v11.safetensors https://civitai.com/api/download/models/107472
 
-echo "Downloading ControlNet models"
-mkdir -p /workspace/stable-diffusion-webui/models/ControlNet
-cd /workspace/stable-diffusion-webui/models/ControlNet
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_openpose.pth
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny.pth
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1p_sd15_depth.pth
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_inpaint.pth
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_lineart.pth
-wget https://huggingface.co/ioclab/ioc-controlnet/resolve/main/models/control_v1p_sd15_brightness.safetensors
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1e_sd15_tile.pth
+# echo "Downloading ControlNet models"
+# mkdir -p /workspace/stable-diffusion-webui/models/ControlNet
+# cd /workspace/stable-diffusion-webui/models/ControlNet
+# wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_openpose.pth
+# wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny.pth
+# wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1p_sd15_depth.pth
+# wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_inpaint.pth
+# wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_lineart.pth
+# wget https://huggingface.co/ioclab/ioc-controlnet/resolve/main/models/control_v1p_sd15_brightness.safetensors
+# wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1e_sd15_tile.pth
 
-echo "Downloading Upscalers"
-cd /workspace/stable-diffusion-webui/models/ESRGAN
-wget https://huggingface.co/ashleykleynhans/upscalers/resolve/main/4x-UltraSharp.pth
-wget https://huggingface.co/ashleykleynhans/upscalers/resolve/main/lollypop.pth
+# echo "Downloading Upscalers"
+# cd /workspace/stable-diffusion-webui/models/ESRGAN
+# wget https://huggingface.co/ashleykleynhans/upscalers/resolve/main/4x-UltraSharp.pth
+# wget https://huggingface.co/ashleykleynhans/upscalers/resolve/main/lollypop.pth
 
 echo "Creating log directory"
 mkdir -p /workspace/logs
